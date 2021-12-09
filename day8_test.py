87201e699108486ef3e103c7b01b4b253eba4bf2
Benoit Pasquereau <bpasquereau@gmail.com>
2021-12-09 09:01:01 +0000

    Day 9

--- day8_test.py
+++ day8_test.py
@@ -1,7 +1,7 @@
 import unittest
 import day8
 
-class TestDay4(unittest.TestCase):
+class TestDay8(unittest.TestCase):
 
     def test_part2(self):
         self.assertEqual(936117, day8.part2("day8_input_test.txt"))
---
+++ day9.py
@@ -0,0 +1,41 @@
+def parse_input(filename):
+    with open(filename) as file:
+        lines = file.read().splitlines()
+        rows = [[int(h) for h in l] for l in lines]
+
+    return rows
+
+heightmap = parse_input("day9_input.txt")
+
+def neighbours(x,y):
+    up = [(x,y - 1)] if y > 0 else []
+    down =[(x, y + 1)] if y < (len(heightmap) - 1) else []
+    left = [(x - 1, y)] if x > 0 else []
+    right = [(x + 1, y)] if x < (len(heightmap[y]) - 1) else []
+    return up + down + left + right
+
+def low_point(x,y):
+    r = True
+    h = heightmap[y][x]
+    for (nx,ny) in neighbours(x,y):
+        r = r and h < heightmap[ny][nx]
+    return r
+
+def basin(x,y, known_points):
+    new_known_points = known_points.union({(x,y)})
+    nb = set(neighbours(x,y)).difference(known_points)
+    for (p_x,p_y) in nb:
+        if heightmap[y][x] < heightmap[p_y][p_x] and heightmap[p_y][p_x] < 9:
+            new_known_points.update(basin(p_x,p_y, new_known_points))
+    return new_known_points
+
+low_points = [(x,y) for y in range(len(heightmap)) for x in range(len(heightmap[y])) if low_point(x,y)]
+low_heights = [int(heightmap[y][x]) + 1 for (x,y) in low_points]
+part1 = sum(low_heights)
+basins = sorted([len(basin(x,y,set())) for (x,y) in low_points])
+part2 = basins[-1]*basins[-2]*basins[-3]
+
+if __name__ == '__main__':
+    print("part1=", part1)
+    print("part2=", part2)
+    
