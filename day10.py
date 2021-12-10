939d2329301b6a4736aebb6bad5955aa459dc4f5
Benoit Pasquereau <bpasquereau@gmail.com>
2021-12-10 08:08:30 +0000

    day 10 part2

--- day10.py
+++ day10.py
@@ -1,3 +1,5 @@
+import functools
+
 def parse_input(filename):
     with open(filename) as file:
         lines = file.read().splitlines()
@@ -14,20 +16,31 @@ def fig(line):
             stack.append(op[c])
         elif c in set(op.values()):
             if stack.pop() != c:
-                return c
-    return None
+                return (c,None)
+    return (None, stack)
 
-def part1(filename):
+def score(stack):
+    points = { ')' : 1, ']' : 2, '}' : 3, '>' : 4 }
+    stack.reverse()
+    return functools.reduce(lambda a, c: a * 5 + points[c], stack, 0)
+
+def part12(filename):
     lines = parse_input(filename)
     r = dict(zip(op.values(), [0] * len(op.values())))
+    scores = []
     for l in lines:
-        c = fig(l)
-        if c:
-            r[c] = 1 + r[c]
+        (ic, stack) = fig(l)
+        if ic:
+            r[ic] = 1 + r[ic]
+        if stack:
+            scores.append(score(stack))
 
     part1 = 3 * r[')'] + 57 * r[']'] + 1197 * r['}'] + 25137 * r['>']
-    return part1
+    scores.sort()
+    part2 = scores[int(len(scores)/2)]
+    return part1, part2
 
 if __name__ == '__main__':
-    print("part1=", part1("day10_input.txt"))
-    
+    r = part12("day10_input.txt")
+    print("part1=", r[0])
+    print("part2=", r[1])
--- day10_test.py
+++ day10_test.py
@@ -3,11 +3,15 @@ import day10
 
 class TestDay10(unittest.TestCase):
     def setUp(self):
-        r = day10.part1("day10_input_test.txt")
-        self.part1 = r
+        r = day10.part12("day10_input_test.txt")
+        self.part1 = r[0]
+        self.part2 = r[1]
 
     def test_part1(self):
         self.assertEqual(26397, self.part1)
+
+    def test_part2(self):
+        self.assertEqual(288957, self.part2)
 
 if __name__ == '__main__':
     unittest.main()
